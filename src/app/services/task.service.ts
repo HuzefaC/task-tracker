18cf6dab5d28e43ffd07475fa5c637dd5e798e1d
huzefa <huzefa@incubyte.co>
2022-06-23 11:35:52 +0530

    Add http client to task service and fetch data from mock server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
