9f4ad0d4e75ea8af7e36cb47ae994f5430a256f9
huzefa <huzefa@incubyte.co>
2022-06-23 23:10:42 +0530

    feat: add new component add-task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     ButtonsComponent,
     TasksComponent,
     TaskItemComponent,
+    AddTaskComponent,
   ],
   imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
   providers: [],
