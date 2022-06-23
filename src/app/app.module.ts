2a493df97846b8913c5f9a0b877e6e7a0192239b
huzefa <huzefa@incubyte.co>
2022-06-23 10:17:04 +0530

    feat: add component tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
