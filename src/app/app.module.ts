caf66c7145a5c5694549a11a4e83fd890f3ecd73
huzefa <huzefa@incubyte.co>
2022-06-23 23:51:10 +0530

    feat: add option to add new task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ButtonsComponent } from './components/buttons/buttons.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     TaskItemComponent,
     AddTaskComponent,
   ],
-  imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
+  text: string;
+  day: string;
+  reminder: boolean = false;
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter<Task>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+
+    if (!this.day) {
+      alert('Please add a date and time');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day.split('T').join(' at '),
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
-
 }
